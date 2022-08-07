416249933077bcaf0bf8eacf02fb9f6bbf8ba494
vakur1 <temavakurov2@icloud.com>
2022-08-07 15:00:18 +0300

    Подготовка проекта

--- app/src/main/java/com/example/chattelegram/MainActivity.kt
+++ app/src/main/java/com/example/chattelegram/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.chattelegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chattelegram.databinding.ActivityMainBinding
+
+
+private lateinit var mBinding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        String // STOPSHIP:
-        String // STOPSHIP:
-        String // STOPSHIP:
     }
 }
